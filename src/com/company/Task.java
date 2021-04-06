56b21ab01e1c70f5d771ba7341e5a3122d7a5223
SEM <samerx777222@gmail.com>
2021-04-06 14:00:56 +0300

    Class with HW

---
+++ src/com/company/Task.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Task {
+
+    /**
+     * HomeTask1
+     * Implement the Strategy pattern for compressing the incoming OutputStream.
+     * <p>
+     * - The output should be a compressed OutputStream depending on the zip/gzip algorithm chosen
+     * <p>
+     * - Use lambda constructions
+     * <p>
+     * - How the data source is selected, the choice of the algorithm (interactive console, launch, config, ..) at your discretion
+     * <p>
+     * Note: for the zip algorithm, be sure to specify the file name in the archive:
+     * <p>
+     * ((ZipOutputStream) out).putNextEntry(new ZipEntry(..));
+     */
+
+}
